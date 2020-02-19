af11c93f1d7e5173721942f2d13011be6c83a30c
Hannes Kimara <hanneskimara@gmail.com>
2020-02-19 09:01:12 +0300

    Create Procfile

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
     '''
